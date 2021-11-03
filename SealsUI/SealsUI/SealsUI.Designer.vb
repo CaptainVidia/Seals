a98d4ef381225bdc468b9ea3e106a5ba3640f34b
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2021-11-03 09:38:15 -0500

    Seals accept CSV files

--- SealsUI/SealsUI/SealsUI.Designer.vb
+++ SealsUI/SealsUI/SealsUI.Designer.vb
@@ -25,23 +25,38 @@ Partial Class SealsUI
         Me.btnExit = New System.Windows.Forms.Button()
         Me.PictureBox = New System.Windows.Forms.PictureBox()
         Me.btnImport = New System.Windows.Forms.Button()
-        Me.btnDeleteBox = New System.Windows.Forms.Button()
         Me.btnSave = New System.Windows.Forms.Button()
-        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SaveFileDialog = New System.Windows.Forms.SaveFileDialog()
         Me.grpBox = New System.Windows.Forms.GroupBox()
-        Me.btnRadMale = New System.Windows.Forms.RadioButton()
-        Me.btnRadFemale = New System.Windows.Forms.RadioButton()
         Me.btnRadPup = New System.Windows.Forms.RadioButton()
+        Me.btnRadFemale = New System.Windows.Forms.RadioButton()
+        Me.btnRadMale = New System.Windows.Forms.RadioButton()
+        Me.dataGridView = New System.Windows.Forms.DataGridView()
+        Me.image = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.image_path = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.xmin = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ymin = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.xmax = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ymax = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.label = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.confidence = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.x_size = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.y_size = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.uploadCSV = New System.Windows.Forms.Button()
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.grpBox.SuspendLayout()
+        CType(Me.dataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btnExit
         '
         Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
-        Me.btnExit.Location = New System.Drawing.Point(704, 406)
+        Me.btnExit.Location = New System.Drawing.Point(674, 596)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(143, 64)
+        Me.btnExit.Size = New System.Drawing.Size(178, 64)
         Me.btnExit.TabIndex = 2
         Me.btnExit.Text = "Exit"
         Me.btnExit.UseVisualStyleBackColor = True
@@ -50,78 +65,46 @@ Partial Class SealsUI
         '
         Me.PictureBox.Location = New System.Drawing.Point(32, 37)
         Me.PictureBox.Name = "PictureBox"
-        Me.PictureBox.Size = New System.Drawing.Size(530, 472)
+        Me.PictureBox.Size = New System.Drawing.Size(557, 472)
         Me.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox.TabIndex = 1
         Me.PictureBox.TabStop = False
         '
         'btnImport
         '
-        Me.btnImport.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
-        Me.btnImport.Location = New System.Drawing.Point(704, 37)
+        Me.btnImport.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnImport.Location = New System.Drawing.Point(58, 599)
         Me.btnImport.Name = "btnImport"
-        Me.btnImport.Size = New System.Drawing.Size(143, 64)
+        Me.btnImport.Size = New System.Drawing.Size(168, 64)
         Me.btnImport.TabIndex = 1
-        Me.btnImport.Text = "Upload"
+        Me.btnImport.Text = "Upload Picture"
         Me.btnImport.UseVisualStyleBackColor = True
-        '
-        'btnDeleteBox
-        '
-        Me.btnDeleteBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
-        Me.btnDeleteBox.Location = New System.Drawing.Point(704, 349)
-        Me.btnDeleteBox.Name = "btnDeleteBox"
-        Me.btnDeleteBox.Size = New System.Drawing.Size(143, 51)
-        Me.btnDeleteBox.TabIndex = 4
-        Me.btnDeleteBox.Text = "Delete Box"
-        Me.btnDeleteBox.UseVisualStyleBackColor = True
         '
         'btnSave
         '
         Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
-        Me.btnSave.Location = New System.Drawing.Point(704, 107)
+        Me.btnSave.Location = New System.Drawing.Point(490, 596)
         Me.btnSave.Name = "btnSave"
-        Me.btnSave.Size = New System.Drawing.Size(143, 64)
+        Me.btnSave.Size = New System.Drawing.Size(178, 64)
         Me.btnSave.TabIndex = 5
-        Me.btnSave.Text = "Save"
+        Me.btnSave.Text = "Save Picture"
         Me.btnSave.UseVisualStyleBackColor = True
         '
-        'SaveFileDialog1
+        'SaveFileDialog
         '
-        Me.SaveFileDialog1.Filter = "save pic|*.jpg"
+        Me.SaveFileDialog.Filter = "save pic|*.jpg"
         '
         'grpBox
         '
         Me.grpBox.Controls.Add(Me.btnRadPup)
         Me.grpBox.Controls.Add(Me.btnRadFemale)
         Me.grpBox.Controls.Add(Me.btnRadMale)
-        Me.grpBox.Location = New System.Drawing.Point(704, 190)
+        Me.grpBox.Location = New System.Drawing.Point(927, 599)
         Me.grpBox.Name = "grpBox"
         Me.grpBox.Size = New System.Drawing.Size(200, 100)
         Me.grpBox.TabIndex = 6
         Me.grpBox.TabStop = False
         Me.grpBox.Text = "Choose a Category"
-        '
-        'btnRadMale
-        '
-        Me.btnRadMale.AutoSize = True
-        Me.btnRadMale.Location = New System.Drawing.Point(23, 19)
-        Me.btnRadMale.Name = "btnRadMale"
-        Me.btnRadMale.Size = New System.Drawing.Size(48, 17)
-        Me.btnRadMale.TabIndex = 0
-        Me.btnRadMale.TabStop = True
-        Me.btnRadMale.Text = "Male"
-        Me.btnRadMale.UseVisualStyleBackColor = True
-        '
-        'btnRadFemale
-        '
-        Me.btnRadFemale.AutoSize = True
-        Me.btnRadFemale.Location = New System.Drawing.Point(23, 42)
-        Me.btnRadFemale.Name = "btnRadFemale"
-        Me.btnRadFemale.Size = New System.Drawing.Size(59, 17)
-        Me.btnRadFemale.TabIndex = 1
-        Me.btnRadFemale.TabStop = True
-        Me.btnRadFemale.Text = "Female"
-        Me.btnRadFemale.UseVisualStyleBackColor = True
         '
         'btnRadPup
         '
@@ -134,22 +117,135 @@ Partial Class SealsUI
         Me.btnRadPup.Text = "Pup"
         Me.btnRadPup.UseVisualStyleBackColor = True
         '
+        'btnRadFemale
+        '
+        Me.btnRadFemale.AutoSize = True
+        Me.btnRadFemale.Location = New System.Drawing.Point(23, 42)
+        Me.btnRadFemale.Name = "btnRadFemale"
+        Me.btnRadFemale.Size = New System.Drawing.Size(59, 17)
+        Me.btnRadFemale.TabIndex = 1
+        Me.btnRadFemale.TabStop = True
+        Me.btnRadFemale.Text = "Female"
+        Me.btnRadFemale.UseVisualStyleBackColor = True
+        '
+        'btnRadMale
+        '
+        Me.btnRadMale.AutoSize = True
+        Me.btnRadMale.Location = New System.Drawing.Point(23, 19)
+        Me.btnRadMale.Name = "btnRadMale"
+        Me.btnRadMale.Size = New System.Drawing.Size(48, 17)
+        Me.btnRadMale.TabIndex = 0
+        Me.btnRadMale.TabStop = True
+        Me.btnRadMale.Text = "Male"
+        Me.btnRadMale.UseVisualStyleBackColor = True
+        '
+        'dataGridView
+        '
+        Me.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.image, Me.image_path, Me.xmin, Me.ymin, Me.xmax, Me.ymax, Me.label, Me.confidence, Me.x_size, Me.y_size})
+        Me.dataGridView.Location = New System.Drawing.Point(595, 37)
+        Me.dataGridView.Name = "dataGridView"
+        Me.dataGridView.Size = New System.Drawing.Size(1042, 472)
+        Me.dataGridView.TabIndex = 7
+        '
+        'image
+        '
+        Me.image.HeaderText = "image"
+        Me.image.Name = "image"
+        '
+        'image_path
+        '
+        Me.image_path.HeaderText = "image_path"
+        Me.image_path.Name = "image_path"
+        '
+        'xmin
+        '
+        Me.xmin.HeaderText = "xmin"
+        Me.xmin.Name = "xmin"
+        '
+        'ymin
+        '
+        Me.ymin.HeaderText = "ymin"
+        Me.ymin.Name = "ymin"
+        '
+        'xmax
+        '
+        Me.xmax.HeaderText = "xmax"
+        Me.xmax.Name = "xmax"
+        '
+        'ymax
+        '
+        Me.ymax.HeaderText = "ymax"
+        Me.ymax.Name = "ymax"
+        '
+        'label
+        '
+        Me.label.HeaderText = "label"
+        Me.label.Name = "label"
+        '
+        'confidence
+        '
+        Me.confidence.HeaderText = "confidence"
+        Me.confidence.Name = "confidence"
+        '
+        'x_size
+        '
+        Me.x_size.HeaderText = "x_size"
+        Me.x_size.Name = "x_size"
+        '
+        'y_size
+        '
+        Me.y_size.HeaderText = "y_size"
+        Me.y_size.Name = "y_size"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(1133, 602)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(143, 64)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Delete Box"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'uploadCSV
+        '
+        Me.uploadCSV.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.uploadCSV.Location = New System.Drawing.Point(232, 599)
+        Me.uploadCSV.Name = "uploadCSV"
+        Me.uploadCSV.Size = New System.Drawing.Size(157, 64)
+        Me.uploadCSV.TabIndex = 9
+        Me.uploadCSV.Text = "Upload CSV"
+        Me.uploadCSV.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.FileName = "OpenFileDialog"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
         'SealsUI
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(944, 561)
+        Me.ClientSize = New System.Drawing.Size(1754, 754)
+        Me.Controls.Add(Me.uploadCSV)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.dataGridView)
         Me.Controls.Add(Me.grpBox)
         Me.Controls.Add(Me.btnSave)
-        Me.Controls.Add(Me.btnDeleteBox)
         Me.Controls.Add(Me.btnImport)
         Me.Controls.Add(Me.PictureBox)
         Me.Controls.Add(Me.btnExit)
         Me.Name = "SealsUI"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Seals UI"
         CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.grpBox.ResumeLayout(False)
         Me.grpBox.PerformLayout()
+        CType(Me.dataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -157,11 +253,25 @@ Partial Class SealsUI
     Friend WithEvents btnExit As Button
     Friend WithEvents PictureBox As PictureBox
     Friend WithEvents btnImport As Button
-    Friend WithEvents btnDeleteBox As Button
     Friend WithEvents btnSave As Button
-    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents SaveFileDialog As SaveFileDialog
     Friend WithEvents grpBox As GroupBox
     Friend WithEvents btnRadPup As RadioButton
     Friend WithEvents btnRadFemale As RadioButton
     Friend WithEvents btnRadMale As RadioButton
+    Friend WithEvents dataGridView As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents uploadCSV As Button
+    Friend WithEvents image As DataGridViewTextBoxColumn
+    Friend WithEvents image_path As DataGridViewTextBoxColumn
+    Friend WithEvents xmin As DataGridViewTextBoxColumn
+    Friend WithEvents ymin As DataGridViewTextBoxColumn
+    Friend WithEvents xmax As DataGridViewTextBoxColumn
+    Friend WithEvents ymax As DataGridViewTextBoxColumn
+    Friend WithEvents label As DataGridViewTextBoxColumn
+    Friend WithEvents confidence As DataGridViewTextBoxColumn
+    Friend WithEvents x_size As DataGridViewTextBoxColumn
+    Friend WithEvents y_size As DataGridViewTextBoxColumn
+    Friend WithEvents OpenFileDialog As OpenFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
 End Class
--- SealsUI/SealsUI/SealsUI.vb
+++ SealsUI/SealsUI/SealsUI.vb
@@ -1,5 +1,7 @@
 ﻿'Author: George Tang
 'Date: 10-6-2021
+Imports System.IO
+Imports System.Text
 Public Class SealsUI
     Dim Draw As Boolean
     Dim DrawColor As Color = Color.Black
@@ -15,24 +17,59 @@ Public Class SealsUI
     End Sub
 
     Private Sub btnImport_Click(sender As Object, e As EventArgs) Handles btnImport.Click
-        Dim opf As New OpenFileDialog
-        opf.Filter = "Choose Image(*.jpg;*.png;*.gif)|*.jpg;*.png;*.gif"
-
-        If opf.ShowDialog = Windows.Forms.DialogResult.OK Then
-            PictureBox.Image = Image.FromFile(opf.FileName)
+        If OpenFileDialog.ShowDialog = DialogResult.OK Then
+            PictureBox.ImageLocation = OpenFileDialog.FileName
         End If
+        OpenFileDialog.FileName = ""
     End Sub
 
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
 
-        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
-            PictureBox.Image.Save(SaveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg)
+        If SaveFileDialog.ShowDialog() = DialogResult.OK Then
+            PictureBox.Image.Save(SaveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg)
         End If
     End Sub
 
     Private Sub btnCreateBox_Click(sender As Object, e As EventArgs)
-        Dim result As Integer
-        result = CheckBox.Show("Male Female Pup", "Choose a Category", CheckBoxRenderer)
+
+    End Sub
+    'Dim opf As New OpenFileDialog
+    'opf.Filter = "Choose Image(*.jpg;*.png;*.gif)|*.jpg;*.png;*.gif"
+    Private Sub uploadCSV_Click(sender As Object, e As EventArgs) Handles uploadCSV.Click
+        dataGridView.Rows.Clear()
+        'Dim opf As New OpenFileDialog
+        'opf.Filter = "Choose( *.csv file)"
+        Try
+            Dim fname As String = "Detection_Results.csv"
+            Dim reader As New StreamReader(fname, Encoding.Default)
+            Dim sline As String = ""
+            Dim colsexpected As Integer = 10
+            Dim r As Integer = 0
+            sline = reader.ReadLine
+            Do
+                sline = reader.ReadLine
+                If sline Is Nothing Then Exit Do
+                Dim words() As String = sline.Split(",")
+                dataGridView.Rows.Add()
+                For i As Integer = 0 To 9
+                    dataGridView.Rows(r).Cells(i).Value = words(i)
+                Next
+                r = r + 1
+            Loop
+            reader.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub SealsUI_FormClosing(sender As Object, ByVal e As FormClosingEventArgs) Handles MyBase.FormClosing
+        'verify if user wants to quit
+        Dim dialog As DialogResult
+        dialog = MessageBox.Show("Are you sure you wan to quit", "Exit", MessageBoxButtons.YesNo)
+        If dialog = DialogResult.No Then
+            e.Cancel = True
+        End If
     End Sub
     'store csv into an array
     'draw on picture and do a redo function
